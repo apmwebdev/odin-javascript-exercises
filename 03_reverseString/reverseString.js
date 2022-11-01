1b0773be551babb7370886734007e47ef6140bee
Austin Miller <austinmillerwebdev@gmail.com>
2022-10-31 18:25:50 -0700

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  const returnStr = str.split('').reverse().join('');
+  return returnStr;
 };
 
 // Do not edit below this line
