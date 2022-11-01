3f0c6a5fb136baf1c2f43c278717de687bcc3bd7
Austin Miller <austinmillerwebdev@gmail.com>
2022-10-31 19:18:07 -0700

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  if (typeof num1 !== 'number' || typeof num2 !== 'number' || num1 < 0 ||
+    num2 < 0) {
+    return 'ERROR';
+  }
+  let sum = 0;
+  let firstNum;
+  let lastNum;
+  if (num1 > num2) {
+    firstNum = num2;
+    lastNum = num1;
+  } else {
+    firstNum = num1;
+    lastNum = num2;
+  }
+  for (let i = firstNum; i <= lastNum; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
