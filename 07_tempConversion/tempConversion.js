96242ae04cc3f8ffa907f1d2bce601d1bb70d140
Austin Miller <austinmillerwebdev@gmail.com>
2022-10-31 19:35:56 -0700

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(tempInFar) {
+  return Math.round((tempInFar - 32) / 1.8 * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(tempInCels) {
+  return Math.round((tempInCels * 1.8 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
