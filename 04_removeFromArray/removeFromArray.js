6827a0766e269f0bce1ec76fbffe1e6352af19a8
Austin Miller <austinmillerwebdev@gmail.com>
2022-10-31 19:06:02 -0700

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(values, ...valuesToRemove) {
+  let returnArr = [...values];
+  for (const val of valuesToRemove) {
+    if (returnArr.indexOf(val) !== -1) {
+      returnArr.splice(returnArr.indexOf(val), 1);
+    }
+  }
+  return returnArr;
 };
 
 // Do not edit below this line
