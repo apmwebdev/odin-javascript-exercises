52e833b02b87fe8446bdac41556da2a3dd571bc8
Austin Miller <austinmillerwebdev@gmail.com>
2022-11-01 20:43:11 -0700

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.replace(/\W/g, '').toLowerCase();
+  if (str.length % 2 === 0) {
+    let half1 = str.slice(0,str.length/2);
+    let half2 = str.slice(str.length/2).split('').reverse().join('');
+    return half1 === half2;
+  } else if (str.length % 2 === 1){
+    let half1 = str.slice(0, Math.floor(str.length/2));
+    let half2 = str.slice(half1.length + 1).split('').reverse().join('');
+    return half1 === half2;
+  }
+  return 'ERROR';
 };
 
 // Do not edit below this line
