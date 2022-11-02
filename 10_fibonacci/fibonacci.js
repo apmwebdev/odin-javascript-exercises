18fd788ba979849bc0d940c6c4abaa8406a231c9
Austin Miller <austinmillerwebdev@gmail.com>
2022-11-01 20:57:24 -0700

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  num = parseInt(num);
+  if (num < 1) return 'OOPS';
+  
+  let seq = [];
+  for (let i = 0; i < num; i++) {
+    let seqNum = i;
+    if (i < 2) {
+      seqNum = 1;
+    } else {
+      seqNum = seq[i-1] + seq[i-2];
+    }
+    seq.push(seqNum);
+  }
+  return seq.pop();
 };
 
 // Do not edit below this line
