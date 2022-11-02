fb2dd411a388708a8db8b0ca0144e12a69c1faeb
Austin Miller <austinmillerwebdev@gmail.com>
2022-11-01 21:17:40 -0700

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  const peopleWithAge = people.map(person => {
+    let personWithAge = {...person};
+    if ('yearOfDeath' in person) {
+      personWithAge.age = person.yearOfDeath - person.yearOfBirth;
+    } else {
+      personWithAge.age = new Date().getFullYear() - person.yearOfBirth;
+    }
+    
+    return personWithAge;
+  });
+  const sortedPeople = peopleWithAge.sort((a,b) => {
+    if (a.age > b.age) {
+      return 1;
+    }
+    return -1;
+  });
+  console.log(sortedPeople);
+  return sortedPeople.pop();
 };
 
 // Do not edit below this line
